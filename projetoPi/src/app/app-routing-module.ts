c4616e53c27f2a764cbbcfd406d1663b8967da68
MiguelSerafim710 <272475@unifio.edu.br>
2026-09-04 00:49:34 +0000

    salvo02

--- projetoPi/src/app/app-routing-module.ts
+++ projetoPi/src/app/app-routing-module.ts
@@ -10,6 +10,8 @@ const routes: Routes = [
     path: 'lista-exercicio',
     loadChildren: () => import('./lista-exercicio/lista-exercicio-module').then(m => m.ListaExercicioModule)
   }
+
+  
 ];
 
 @NgModule({
--- projetoPi/src/app/lista-exercicio/lista-exercicio-routing-module.ts
+++ projetoPi/src/app/lista-exercicio/lista-exercicio-routing-module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaExercicioModule } from './lista-exercicio-module';
 import { Ex1 } from './ex1/ex1';
+import { Ex2 } from './ex2/ex2';
 
 const routes: Routes = [
-  {path: 'ex1', component: Ex1}
+  {path: 'ex1', component: Ex1},
+  {path: 'ex2', component: Ex2}
 ];
 
 @NgModule({
